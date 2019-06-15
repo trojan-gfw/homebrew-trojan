81b88745216694353c1b45f0106766a46e23ec46
GreaterFire <32649575+GreaterFire@users.noreply.github.com>
2019-06-15 12:20:28 -0700

    trojan: update to 1.12.3

--- Formula/trojan.rb
+++ Formula/trojan.rb
@@ -1,8 +1,8 @@
 class Trojan < Formula
   desc "An unidentifiable mechanism that helps you bypass GFW."
   homepage "https://trojan-gfw.github.io/trojan/"
-  url "https://github.com/trojan-gfw/trojan/archive/v1.12.2.tar.gz"
-  sha256 "f0d5379787d0aa66a952b1b3f7777f5f8c0b818d5c49b2ca67820076d8abe74b"
+  url "https://github.com/trojan-gfw/trojan/archive/v1.12.3.tar.gz"
+  sha256 "cdb86b6356ee9bb1716dae175ec6b0963a5c896f19bff903f99071a826615f63"
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "openssl@1.1"
@@ -41,7 +41,7 @@ class Trojan < Formula
   end
 
   test do
-    system "git", "clone", "--branch=v1.12.2", "https://github.com/trojan-gfw/trojan.git"
+    system "git", "clone", "--branch=v1.12.3", "https://github.com/trojan-gfw/trojan.git"
     system "sh", "-c", "cd trojan/tests/LinuxSmokeTest && ./basic.sh /usr/local/bin/trojan"
     system "sh", "-c", "cd trojan/tests/LinuxSmokeTest && ./fake-client.sh /usr/local/bin/trojan"
   end
